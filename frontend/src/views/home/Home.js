87a672f0af61348b4447625d823afdf44ae8f2cf
Karmel920 <mikolajpe2@gmail.com>
2023-03-11 23:38:55 +0100

    Home view

---
+++ frontend/src/views/home/Home.js
@@ -0,0 +1,16 @@
+import Header from "../../layout/Header";
+
+function Home() {
+    return (
+        <>
+        <div>
+            <Header />
+        </div>
+        <div className="text-3xl width-full flex justify-center">
+            <h1>Home</h1>
+        </div>
+        </>
+    );
+}
+
+export default Home;
