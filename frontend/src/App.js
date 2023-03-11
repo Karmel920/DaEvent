f5832ac5248c66d02ec368d69cffc73b4952eeaf
Karmel920 <mikolajpe2@gmail.com>
2023-03-11 23:39:54 +0100

    Update routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router';
-import './App.css';
-import Home from './views/Home';
+import Home from './views/home/Home';
+import Login from "./views/login/Login";
 
 function App() {
   return (
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
   );
 }
