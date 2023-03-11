3815131667e640d32e1e3cf2404048f2fbcec891
Karmel920 <mikolajpe2@gmail.com>
2023-03-11 23:39:11 +0100

    Header layout

---
+++ frontend/src/layout/Header.js
@@ -0,0 +1,11 @@
+
+function Header() {
+    return (
+        <div className="text-color-error width-full flex justify-between">
+            <h1 className="text-3xl">Logo</h1>
+            <p className="text-xl">Profile</p>
+        </div>
+    );
+}
+
+export default Header;
