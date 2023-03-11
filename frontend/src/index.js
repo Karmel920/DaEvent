ea2e36921277d372ac6c52e55b58d2fa8ec3a828
Karmel920 <mikolajpe2@gmail.com>
2023-03-11 23:39:37 +0100

    Add mantine provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
+import {MantineProvider, createEmotionCache} from "@mantine/core";
+
+const myCache = createEmotionCache({ key: 'mantine' });
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <BrowserRouter>
-          <App />
+          <MantineProvider emotionCache={myCache}>
+              <App />
+          </MantineProvider>
       </BrowserRouter>
   </React.StrictMode>
 );
