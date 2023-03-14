5f585f701eeffac3a42bbc82a7c0e52418d0dbc1
Karmel920 <mikolajpe2@gmail.com>
2023-03-15 00:12:52 +0100

    Add features to Login view

--- frontend/src/views/login/Login.js
+++ frontend/src/views/login/Login.js
@@ -1,5 +1,6 @@
 import Header from "../../layout/Header";
 import {Button, TextInput} from "@mantine/core";
+import {Link} from "react-router-dom"
 
 function Login() {
     return (
@@ -7,29 +8,44 @@ function Login() {
             <div>
                 <Header />
             </div>
-            <div className="mx-auto mt-[25vh] max-w-md shadow-lg bg-color-dark-medium rounded-lg px-8">
-                <form className="flex flex-col p-3 py-8">
-                    <TextInput
-                        labelProps={{ style: { color: '#8b8b8b' } }}
-                        placeholder="Enter your login"
-                        label="Login"
-                        radius="lg"
-                    />
-                    <TextInput
-                        labelProps={{ style: { color: '#8b8b8b' } }}
-                        placeholder="Enter your password"
-                        label="Password"
-                        radius="lg"
-                    />
-                    <Button radius="lg" size="md" color="indigo"
-                            type={"submit"}
-                            fullWidth
-                            mt={20}
-                            mb={0}
-                            sx={{backgroundColor: "#71c6dd", "&:hover": {backgroundColor: "#5698a8"}, textColor: "#8b8b8b"}}>
-                        Login
-                    </Button>
-                </form>
+            <div className="mx-auto mt-[5vh] max-w-md shadow-lg bg-color-dark rounded-lg h-[80vh]">
+                <div className="bg-color-dark-light w-full px-8 flex justify-center text-color-light py-3 rounded-t-lg">
+                    LOGIN
+                </div>
+                <p className="text-color-main flex justify-center pt-6">
+                    Find your ideal project
+                </p>
+                <div className="px-8">
+                    <form className="flex flex-col p-3 py-5 gap-3 justify-center">
+                        <TextInput
+                            labelProps={{ style: { color: '#b2bdbd' } }}
+                            placeholder="Enter your login"
+                            label="Login"
+                            radius="lg"
+                        />
+                        <TextInput
+                            labelProps={{ style: { color: '#b2bdbd' } }}
+                            placeholder="Enter your password"
+                            label="Password"
+                            radius="lg"
+                        />
+                        <Button radius="lg" size="md" color="color-main.3"
+                                type={"submit"}
+                                mt={30}
+                                mb={0}
+                                >
+                            Login
+                        </Button>
+                    </form>
+                </div>
+                <p className="text-color-light-gray flex justify-center pt-6">
+                    Haven't signed up yet?
+                </p>
+                <Link to='/register'>
+                    <p className="text-color-main flex justify-center cursor-pointer">
+                        Sign up
+                    </p>
+                </Link>
             </div>
         </>
     );
