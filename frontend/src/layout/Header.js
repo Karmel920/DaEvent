936aace2178299a7d4021142f2f8456aeaab26db
Karmel920 <mikolajpe2@gmail.com>
2023-03-15 00:09:48 +0100

    Add logo and user's avatar

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,9 +1,24 @@
+import {IoIosPeople} from "react-icons/io"
+import avatar from "../images/icons/avatar.svg"
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
-        <div className="text-color-error width-full flex justify-between">
-            <h1 className="text-3xl">Logo</h1>
-            <p className="text-xl">Profile</p>
+        <div className="bg-color-dark width-full flex justify-between">
+            <div className="flex justify-center align-middle mb-3 mt-1">
+                <Link to='/'>
+                    <IoIosPeople size="3em" className="text-color-main ml-10 cursor-pointer"></IoIosPeople>
+                </Link>
+                <Link to='/'>
+                    <p className="text-xl pl-5 pt-2.5 font-semibold text-color-light cursor-pointer">DaEvent</p>
+                </Link>
+            </div>
+            <div className="w-50 h-12 pt-2.5 flex gap-4 pr-20">
+                <img src={avatar} alt="Avatar" className="mr-30 cursor-pointer"/>
+                <Link to='/login'>
+                    <p className="pt-2 text-color-gray cursor-pointer">Login</p>
+                </Link>
+            </div>
         </div>
     );
 }
