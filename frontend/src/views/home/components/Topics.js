1d7da62a8d9f78679b8b2ae297c8ebf4ccc5d8a3
Karmel920 <mikolajpe2@gmail.com>
2023-03-16 00:27:07 +0100

    Add gap in list

--- frontend/src/views/home/components/Topics.js
+++ frontend/src/views/home/components/Topics.js
@@ -2,12 +2,12 @@ import {BsChevronDown} from "react-icons/bs";
 
 function Topics() {
     return (
-        <div className="topics ml-16 mt-10">
+        <div className="topics mt-10">
             <div className="topics-header">
                 <h2 className="text-2xl text-color-dark-light">Browse Topics</h2>
             </div>
-            <div className="text-lg flex flex-col gap-5 cursor-pointer">
-                <ul className="topics-list text-color-light-gray">
+            <div className="text-lg flex flex-col gap-5 cursor-pointer mt-8">
+                <ul className="topics-list text-color-light-gray flex flex-col gap-8">
                     <li>
                         All 13
                     </li>
@@ -21,7 +21,7 @@ function Topics() {
                         React 2
                     </li>
                 </ul>
-                <a className="flex text-color-main">
+                <a className="flex text-color-main mt-4">
                     More
                     <BsChevronDown className="mt-1.5"/>
                 </a>
