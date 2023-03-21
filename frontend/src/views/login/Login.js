1dc56a3afde871edb078e2c7a807ea7ccf0e9968
Karmel920 <mikolajpe2@gmail.com>
2023-03-21 14:39:29 +0100

    Add logo

--- frontend/src/views/login/Login.js
+++ frontend/src/views/login/Login.js
@@ -1,11 +1,14 @@
-import Header from "../../layout/Header";
 import {Button, TextInput} from "@mantine/core";
 import {Link} from "react-router-dom"
+import {IoIosPeople} from "react-icons/io";
 
 function Login() {
     return (
         <>
-            <div className="mx-auto mt-[10vh] max-w-md shadow-lg bg-color-dark rounded-lg h-[80vh]">
+            <div className="flex justify-center align-middle">
+                <IoIosPeople size="5em" className="text-color-main cursor-pointer"></IoIosPeople>
+            </div>
+            <div className="mx-auto max-w-md shadow-lg bg-color-dark rounded-lg h-[80vh]">
                 <div className="bg-color-dark-light w-full px-8 flex justify-center text-color-light py-3 rounded-t-lg">
                     LOGIN
                 </div>
@@ -15,27 +18,25 @@ function Login() {
                 <div className="px-8">
                     <form className="flex flex-col p-3 py-5 gap-3 justify-center">
                         <TextInput
-                            labelProps={{ style: { color: '#b2bdbd' } }}
                             placeholder="Enter your login"
                             label="Login"
                             radius="sm"
                         />
                         <TextInput
-                            labelProps={{ style: { color: '#b2bdbd' } }}
                             placeholder="Enter your password"
                             label="Password"
                             radius="sm"
                         />
-                        <Button radius="sm" size="md" color="color-main.3"
+                        <Button radius="sm" size="md" color="color-main.4"
                                 type={"submit"}
-                                mt={30}
+                                mt={20}
                                 mb={0}
                                 >
                             Login
                         </Button>
                     </form>
                 </div>
-                <p className="text-color-light-gray flex justify-center pt-6">
+                <p className="text-color-light-gray flex justify-center pt-4">
                     Haven't signed up yet?
                 </p>
                 <Link to='/register'>
--- frontend/src/views/register/Register.js
+++ frontend/src/views/register/Register.js
@@ -1,11 +1,14 @@
-import Header from "../../layout/Header";
 import {Button, TextInput} from "@mantine/core";
 import {Link} from "react-router-dom";
+import {IoIosPeople} from "react-icons/io";
 
 function Register() {
     return (
         <>
-            <div className="mx-auto mt-[10vh] max-w-md shadow-lg bg-color-dark rounded-lg h-[80vh]">
+            <div className="flex justify-center align-middle">
+                <IoIosPeople size="5em" className="text-color-main cursor-pointer"></IoIosPeople>
+            </div>
+            <div className="mx-auto max-w-md shadow-lg bg-color-dark rounded-lg h-[80vh]">
                 <div className="bg-color-dark-light w-full px-8 flex justify-center text-color-light py-3 rounded-t-lg">
                     REGISTER
                 </div>
@@ -15,33 +18,30 @@ function Register() {
                 <div className="px-8">
                     <form className="flex flex-col p-3 py-5 gap-3 justify-center">
                         <TextInput
-                            labelProps={{ style: { color: '#b2bdbd' } }}
                             placeholder="Enter your login"
                             label="Login"
                             radius="sm"
                         />
                         <TextInput
-                            labelProps={{ style: { color: '#b2bdbd' } }}
                             placeholder="Enter your password"
                             label="Password"
                             radius="sm"
                         />
                         <TextInput
-                            labelProps={{ style: { color: '#b2bdbd' } }}
                             placeholder="Confirm your password"
                             label="Password confirmation"
                             radius="sm"
                         />
-                        <Button radius="sm" size="md" color="color-main.3"
+                        <Button radius="sm" size="md" color="color-main.4"
                                 type={"submit"}
-                                mt={30}
+                                mt={20}
                                 mb={0}
                         >
                             Register
                         </Button>
                     </form>
                 </div>
-                <p className="text-color-light-gray flex justify-center pt-6">
+                <p className="text-color-light-gray flex justify-center pt-4">
                     Already signed up?
                 </p>
                 <Link to='/login'>
