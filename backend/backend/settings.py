25cf392cda16107eb99f29782fa2dc8ae1cd8fc2
Karmel920 <mikolajpe2@gmail.com>
2023-03-28 23:55:27 +0200

    Create api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'daevent.apps.DaeventConfig',
+
     'rest_framework',
     'corsheaders',
+    'daevent',
 ]
 
 MIDDLEWARE = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from daevent import views
-
-router = routers.DefaultRouter()
-router.register(r'topics', views.TopicView, 'topic')
-router.register(r'projects', views.ProjectView, 'project')
-router.register(r'users', views.UserDetailsView, 'user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('', include('api.urls')),
 ]
