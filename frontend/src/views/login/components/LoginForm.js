3bac5f8443e8cafa291402747ce8117797b3a4c3
Karmel920 <mikolajpe2@gmail.com>
2023-04-13 12:29:38 +0200

    Create login with token

---
+++ frontend/src/views/login/components/LoginForm.js
@@ -0,0 +1,63 @@
+import {Alert, Button, TextInput} from "@mantine/core";
+import {useForm} from "@mantine/form";
+import {useMutation} from "react-query";
+import {api} from "../../../api/ApiServices";
+import {BiErrorCircle} from "react-icons/bi";
+import {useAuth} from "../../../context/AuthContext";
+import {useNavigate} from "react-router";
+
+function LoginForm() {
+    const form = useForm({
+        initialValues: {
+            email: '',
+            password: '',
+        }
+    });
+
+    const {login} = useAuth();
+
+    const submitHandle = data => {
+        loginMutation.mutate(data)
+    };
+
+    const navigate = useNavigate();
+
+    const onSuccess = (data) => {
+        login(data.data.access);
+        navigate('/');
+    }
+
+    const loginMutation = useMutation(api.login, {
+        onSuccess: data => onSuccess(data),
+    });
+
+    return (
+        <form className="flex flex-col p-3 py-5 gap-3 justify-center" onSubmit={form.onSubmit(submitHandle)}>
+            {loginMutation.isError && <Alert icon={<BiErrorCircle size="1rem"/>} title="Wrong email or password!" color="color-error.6" variant="outline" pt={20}/>}
+            <TextInput
+                placeholder="Enter your email"
+                label="Email"
+                radius="sm"
+                type={"email"}
+                {...form.getInputProps('email')}
+            />
+            <TextInput
+                placeholder="Enter your password"
+                label="Password"
+                radius="sm"
+                type={"password"}
+                {...form.getInputProps('password')}
+            />
+            <Button radius="sm" size="md" color="color-main.4"
+                    type={"submit"}
+                    mt={20}
+                    mb={0}
+                    loading={loginMutation.isLoading}
+            >
+                Login
+            </Button>
+        </form>
+    );
+}
+
+export default LoginForm;
