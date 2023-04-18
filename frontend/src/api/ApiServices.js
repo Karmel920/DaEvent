1240c1d2344b2b2cca11ed1affe8584c951e78b4
Karmel920 <mikolajpe2@gmail.com>
2023-04-19 01:50:36 +0200

    Add new endpoints

--- frontend/src/api/ApiServices.js
+++ frontend/src/api/ApiServices.js
@@ -7,4 +7,25 @@ export const api = {
     topics: () => {
         return axios.get(`${process.env.REACT_APP_API_URL}/api/topics`)
     },
+    allTopics: () => {
+        return axios.get(`${process.env.REACT_APP_API_URL}/api/all-topics`)
+    },
+    getAllProjects: page => {
+        return axios.get(`${process.env.REACT_APP_API_URL}/api/projects?page=${page}`)
+    },
+    getProjectById: id => {
+        return axios.get(`${process.env.REACT_APP_API_URL}/api/project/${id}`)
+    },
+    getProjectsByTopic: ({ name, page }) => {
+        return axios.get(`${process.env.REACT_APP_API_URL}/api/projects/${name}?page=${page}`)
+    },
+    getRecentActivities: () => {
+        return axios.get(`${process.env.REACT_APP_API_URL}/api/recent-activities`)
+    },
+    getProjectActivities: id => {
+        return axios.get(`${process.env.REACT_APP_API_URL}/api/project-activities/${id}`)
+    },
+    getParticipants: id => {
+        return axios.get(`${process.env.REACT_APP_API_URL}/api/project-participants/${id}`)
+    },
 }
