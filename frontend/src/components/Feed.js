fdab3655f58f7aa59c186781d086a009297d97f0
Karmel920 <mikolajpe2@gmail.com>
2023-04-19 01:52:43 +0200

    Display projects feed from backend

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -1,68 +1,109 @@
 import avatar from "../public/images/icons/avatar.svg"
 import {AiOutlineTeam} from "react-icons/ai";
-import {Avatar} from "@mantine/core";
-import {Link} from "react-router-dom";
+import {Avatar, Pagination} from "@mantine/core";
+import {Link, useParams, useSearchParams, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useMutation} from "react-query";
+import {api} from "../api/ApiServices";
 
-function Feed() {
+
+function Feed({setTotalHandler}) {
+    const {slug} = useParams();
+    const [prevSlug, setPrevSlug] = useState(slug);
+    let [searchParams, setSearchParams] = useSearchParams();
+    const [projects, setProjects] = useState([]);
+    const [activePage, setActivePage] = useState(searchParams.get('page') || 1);
+    const [totalProjects, setTotalProjects] = useState(1);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (slug !== prevSlug) {
+            setActivePage(1);
+            setPrevSlug(slug);
+        }
+        if (slug) {
+            projectsByTopicMutation.mutate({name: slug, page: activePage});
+        }
+        else {
+            allProjectsMutation.mutate(activePage);
+        }
+    }, [slug, activePage]);
+
+    const allProjectsMutation = useMutation(api.getAllProjects, {
+        onSuccess: ({data}) => {
+            setProjects(data.results);
+            setTotalProjects(data.count);
+            setTotalHandler(data.count);
+        }
+    });
+
+    const projectsByTopicMutation = useMutation(api.getProjectsByTopic, {
+        onSuccess: ({data}) => {
+            setProjects(data.results);
+            setTotalProjects((data.count));
+            setTotalHandler(data.count);
+        }
+    })
+
+    const handlePageChange = (pageNumber) => {
+        setActivePage(pageNumber);
+        {
+            slug ? (
+                navigate(`/projects/${slug}?page=${pageNumber}`)
+            ) : (
+                navigate(`/projects?page=${pageNumber}`)
+            )
+        }
+        handleScrollToTop();
+    }
+
+    function handleScrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
     return (
         <div>
-            <div className="bg-color-dark p-6 rounded-xl mb-6">
-                <div className="flex justify-between items-center text-color-light">
-                    <Link to="/profile">
-                        <div className="flex items-center">
-                            <Avatar src={avatar} alt="Avatar" size="md"
-                                sx={{
-                                    'cursor': "pointer", 'position': "static"
-                                }}
-                            />
-                            <span className="text-color-main ml-2 cursor-pointer">@Karmel</span>
+            {projects.map(item =>
+                <div key={item.id} className="bg-color-dark p-6 rounded-xl mb-6">
+                    <div className="flex justify-between items-center text-color-light">
+                        <Link to="/profile">
+                            <div className="flex items-center">
+                                <Avatar src={avatar} alt="Avatar" size="md"
+                                        sx={{
+                                            'cursor': "pointer", 'position': "static"
+                                        }}
+                                />
+                                <span className="text-color-main ml-2 cursor-pointer">@{item.host}</span>
+                            </div>
+                        </Link>
+                        <div>
+                            <span>{item.timesince} ago</span>
+                        </div>
+                    </div>
+                    <Link to={`/project/${item.id}`}>
+                        <div className="cursor-pointer text-color-light font-medium text-xl mt-4 mb-6 transition">
+                            <p>{item.name}</p>
                         </div>
                     </Link>
-                    <div>
-                        <span>1 day, 19 hours ago</span>
-                    </div>
-                </div>
-                <Link to='/project'>
-                    <div className="cursor-pointer text-color-light font-medium text-xl mt-4 mb-6 transition">
-                        <p>Python project</p>
-                    </div>
-                </Link>
-                <div className="flex justify-between text-color-light border-solid border-color-gray border-t pt-4">
-                    <div className="flex gap-3 items-center">
-                        <AiOutlineTeam size="2em" className="text-color-main "/>
-                        12 Joined
-                    </div>
-                    <p className="bg-color-dark-medium py-1.5 px-4 rounded-3xl font-medium">Python</p>
-                </div>
-            </div>
-            <div className="bg-color-dark p-6 rounded-xl mb-6">
-                <div className="flex justify-between items-center text-color-light">
-                    <Link to="/profile">
-                        <div className="flex items-center">
-                            <Avatar src={avatar} alt="Avatar" size="md"
-                                    sx={{
-                                        'cursor': "pointer", 'position': "static"
-                                    }}
-                            />
-                            <span className="text-color-main ml-2 cursor-pointer">@Karmel</span>
+                    <div className="flex justify-between text-color-light border-solid border-color-gray border-t pt-4">
+                        <div className="flex gap-3 items-center">
+                            <AiOutlineTeam size="2em" className="text-color-main "/>
+                            {item.participants_count} Joined
                         </div>
-                    </Link>
-                    <div>
-                        <span>1 day, 19 hours ago</span>
+                        <p className="bg-color-main py-1.5 px-4 rounded-3xl font-medium text-color-bg">{item.topic}</p>
                     </div>
                 </div>
-                <Link to='/project'>
-                    <div className="cursor-pointer text-color-light font-medium text-xl mt-4 mb-6 transition">
-                        <p>Python project</p>
-                    </div>
-                </Link>
-                <div className="flex justify-between text-color-light border-solid border-color-gray border-t pt-4">
-                    <div className="flex gap-3 items-center">
-                        <AiOutlineTeam size="2em" className="text-color-main "/>
-                        12 Joined
-                    </div>
-                    <p className="bg-color-dark-medium py-1.5 px-4 rounded-3xl font-medium">Python</p>
-                </div>
+            )}
+            <div className="flex justify-center">
+                <Pagination
+                    value={activePage}
+                    onChange={(pageNumber) => handlePageChange(pageNumber)}
+                    total={totalProjects / 6 + 1}
+                    color="color-dark.5"
+                />
             </div>
         </div>
     );
