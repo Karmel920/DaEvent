922d38720de149935d4217607c67304a4238c4ba
Karmel920 <mikolajpe2@gmail.com>
2023-04-23 01:23:50 +0200

    Move form code to another component

--- frontend/src/views/register/Register.js
+++ frontend/src/views/register/Register.js
@@ -1,6 +1,6 @@
-import {Button, TextInput} from "@mantine/core";
 import {Link} from "react-router-dom";
 import {IoIosPeople} from "react-icons/io";
+import RegisterForm from "./components/RegisterForm";
 
 function Register() {
     return (
@@ -16,40 +16,9 @@ function Register() {
                     Find your ideal project
                 </p>
                 <div className="px-8">
-                    <form className="flex flex-col p-3 py-5 gap-3 justify-center">
-                        <TextInput
-                            placeholder="Enter your email"
-                            label="Email"
-                            radius="sm"
-                            type={"email"}
-                        />
-                        <TextInput
-                            placeholder="Enter your username"
-                            label="Username"
-                            radius="sm"
-                        />
-                        <TextInput
-                            placeholder="Enter your password"
-                            label="Password"
-                            radius="sm"
-                            type={"password"}
-                        />
-                        <TextInput
-                            placeholder="Confirm your password"
-                            label="Password confirmation"
-                            radius="sm"
-                            type={"password"}
-                        />
-                        <Button radius="sm" size="md" color="color-main.4"
-                                type={"submit"}
-                                mt={20}
-                                mb={0}
-                        >
-                            Register
-                        </Button>
-                    </form>
+                    <RegisterForm/>
                 </div>
-                <p className="text-color-light-gray flex justify-center pt-4">
+                <p className="text-color-light-gray flex justify-center pt-2">
                     Already signed up?
                 </p>
                 <Link to='/login'>
