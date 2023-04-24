41049e4b9c5e9e46376e42dc20bbfe78c2044257
Karmel920 <mikolajpe2@gmail.com>
2023-04-24 21:14:08 +0200

    Fix links

--- frontend/src/views/project/components/Participants.js
+++ frontend/src/views/project/components/Participants.js
@@ -1,14 +1,17 @@
 import avatar from "../../../public/images/icons/avatar.svg"
-import {Avatar} from "@mantine/core";
+import {Avatar, Button} from "@mantine/core";
 import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {useMutation} from "react-query";
 import {api} from "../../../api/ApiServices";
+import {BsPersonAdd} from "react-icons/bs";
+import {useAuth} from "../../../context/AuthContext";
 
 function Participants() {
     const {slug} = useParams();
     const [participants, setParticipants] = useState([]);
     const [participantsCount, setParticipantsCount] = useState(0);
+    const {user} = useAuth();
 
     useEffect(() => {
         if (!slug) return;
@@ -29,6 +32,19 @@ function Participants() {
                     <h2 className="text-color-light py-2 px-4">PARTICIPANTS
                         <span className="ml-1.5 text-color-main">({participantsCount} Joined)</span>
                     </h2>
+                </div>
+                <div className="mt-2 flex justify-center">
+                    {participants.some((p) => p.id === user?.id) ? (
+                        <></>
+                    ) : (
+                        <Button color="color-dark-medium.2" radius="md" size="sm"
+                                leftIcon={<BsPersonAdd size="1.2rem" className="mt-0.5 mr-0.5 text-color-dark"/>}
+                                sx={{
+                                    'position': "static",
+                                }}>
+                            <p className="text-color-dark">Join</p>
+                        </Button>
+                    )}
                 </div>
                 <div className="h-full w-full p-4 flex flex-col gap-4">
                     {participants.map(item =>
