470f89f1aeab25666cd42bc0989952d103be2dc7
Karmel920 <mikolajpe2@gmail.com>
2023-04-24 21:09:47 +0200

    Create form for Project model

--- backend/daevent/forms.py
+++ backend/daevent/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import ModelForm
 
-from .models import CustomUser
+from .models import CustomUser, Project
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,3 +14,9 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email", "username", "full_name", "bio")
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ("name", "description")
