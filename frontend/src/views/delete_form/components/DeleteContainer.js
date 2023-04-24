548e6f3d05ea8c3fd1038fc756857812fc582217
Karmel920 <mikolajpe2@gmail.com>
2023-04-24 21:13:50 +0200

    Create view of delete user and project

--- frontend/src/views/delete_form/components/DeleteContainer.js
+++ frontend/src/views/delete_form/components/DeleteContainer.js
@@ -1,8 +1,41 @@
 import {Button} from "@mantine/core";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import {match} from 'path-to-regexp';
 import {IoMdArrowBack} from "react-icons/io";
+import {useAuth} from "../../../context/AuthContext";
+import {useMutation} from "react-query";
+import {api} from "../../../api/ApiServices";
+import {useNavigate} from "react-router";
 
 function DeleteContainer() {
+    const {user} = useAuth();
+    const {logout} = useAuth();
+    const navigate = useNavigate();
+
+    const location = useLocation();
+    const matchProject = match('/delete-project/:slug', {decode: decodeURIComponent})(location.pathname);
+    const matchUser = match('/delete-user', {decode: decodeURIComponent})(location.pathname);
+
+    const submitUserDelete = data => {
+        deleteUserMutation.mutate(data)
+    }
+
+    const submitProjectDelete = data => {
+        deleteProjectMutation.mutate(data)
+    }
+
+    const deleteUserMutation = useMutation(api.deleteUser, {
+        onSuccess: () => {
+            logout();
+        }
+    })
+
+    const deleteProjectMutation = useMutation(api.deleteProject, {
+        onSuccess: () => {
+            navigate("/")
+        }
+    })
+
     return (
         <div className="pt-10">
             <div className="h-[75vh] w-10/12 rounded-lg bg-color-dark mx-auto">
@@ -15,17 +48,39 @@ function DeleteContainer() {
                     </div>
                 </div>
                 <div className="py-6 px-8 flex flex-col gap-2">
-                    <div className="text-color-light-gray text-lg">
-                        Are you sure you want to delete "Project name"?
-                    </div>
-                    <Button radius="sm" size="sm" color="color-main.4"
-                            type={"submit"}
-                            mb={0}
-                            mt={15}
-                            w={100}
-                    >
-                        Confirm
-                    </Button>
+                    {matchProject ? (
+                        <>
+                            <div className="text-color-light-gray text-lg">
+                                Are you sure you want to delete the project?
+                            </div>
+                            <Button radius="sm" size="sm" color="color-error.4"
+                                    onClick={() => submitProjectDelete(matchProject.params.slug)}
+                                    type={"button"}
+                                    mb={0}
+                                    mt={15}
+                                    w={130}
+                            >
+                                Delete project
+                            </Button>
+                        </>
+                    ) : matchUser ? (
+                        <>
+                            <div className="text-color-light-gray text-lg">
+                                Are you sure you want to delete the Account?
+                            </div>
+                            <Button radius="sm" size="sm" color="color-error.4"
+                                    onClick={() => submitUserDelete(user?.id)}
+                                    type={"button"}
+                                    mb={0}
+                                    mt={15}
+                                    w={130}
+                            >
+                                Delete Account
+                            </Button>
+                        </>
+                    ) : (
+                        <div>Invalid URL</div>
+                    )}
                 </div>
             </div>
         </div>
