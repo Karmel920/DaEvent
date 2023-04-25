9827a224fb43b53c09ed7591f4d541d7720cee82
Karmel920 <mikolajpe2@gmail.com>
2023-04-25 02:02:23 +0200

    Move code to Comments component

--- frontend/src/views/project/components/ProjectContainer.js
+++ frontend/src/views/project/components/ProjectContainer.js
@@ -1,19 +1,18 @@
 import avatar from "../../../public/images/icons/avatar.svg"
-import {Avatar, Button, TextInput} from "@mantine/core";
+import {Avatar} from "@mantine/core";
 import {Link, useParams} from "react-router-dom";
 import {IoMdArrowBack, IoMdClose} from "react-icons/io";
 import {AiOutlineEdit} from "react-icons/ai";
-import {BsPersonAdd} from "react-icons/bs";
 import {useEffect, useState} from "react";
 import {useMutation} from "react-query";
 import {api} from "../../../api/ApiServices";
 import {useNavigate} from "react-router";
 import {useAuth} from "../../../context/AuthContext";
+import Comments from "./Comments";
 
 function ProjectContainer() {
     const {slug} = useParams();
     const [project, setProject] = useState({});
-    const [activities, setActivities] = useState([]);
     const navigate = useNavigate();
     const {user} = useAuth();
 
@@ -24,16 +23,11 @@ function ProjectContainer() {
     useEffect(() => {
         if (!slug) return;
         projectMutation.mutate(slug);
-        activitiesMutation.mutate(slug);
     }, [slug]);
 
     const projectMutation = useMutation(api.getProjectById, {
         onSuccess: ({data}) => setProject(data),
     });
-
-    const activitiesMutation = useMutation(api.getProjectActivities, {
-        onSuccess: ({data}) => setActivities(data)
-    })
 
     return (
         <div className="pt-10 h-[90vh]">
@@ -96,7 +90,7 @@ function ProjectContainer() {
                             </div>
                         </div>
                     </div>
-                    <div className="h-32 overflow-auto mb-2">
+                    <div className="h-28 overflow-auto mb-2">
                         <p className="text-xs text-color-gray mb-1">DESCRIPTION</p>
                         <div className="text-color-light-gray mb-2">
                             {project?.description}
@@ -104,44 +98,7 @@ function ProjectContainer() {
                     </div>
                     <div>
                         <p className="text-xs text-color-gray mb-1">COMMENTS</p>
-                        <div className="bg-color-bg h-48 overflow-auto">
-                            <div className="py-2 px-4">
-                                {activities.map(item =>
-                                    <div key={item?.id} className="border-solid border-l border-color-gray mb-2">
-                                        <div className="pl-3 py-2 flex flex-col gap-1">
-                                            <div className="flex items-center justify-between">
-                                                <div className="flex items-center">
-                                                    <Link to={`/profile/${item?.user?.id}`}>
-                                                        <div className="flex leading-normal items-center">
-                                                            <Avatar src={avatar} alt="Avatar" size="sm"
-                                                                    sx={{
-                                                                        'cursor': "pointer", 'position': "static"
-                                                                    }}
-                                                            />
-                                                            <p className="pl-2 text-sm text-color-main cursor-pointer">@{item?.user?.username}</p>
-                                                        </div>
-                                                    </Link>
-                                                    <p className="ml-3 text-sm text-color-light-gray">{item?.timesince} ago</p>
-                                                </div>
-                                                {/*<div>*/}
-                                                {/*    <Link to='/delete'>*/}
-                                                {/*        <IoMdClose className="text-color-light-gray text-sm cursor-pointer mr-2"/>*/}
-                                                {/*    </Link>*/}
-                                                {/*</div>*/}
-                                            </div>
-                                            <p className="text-color-light-gray">{item?.body}</p>
-                                        </div>
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                        <div className="">
-                            <form>
-                                <TextInput placeholder="Write your message here..."
-                                           styles={{input: {backgroundColor: "#696d97"}}}
-                                />
-                            </form>
-                        </div>
+                        <Comments/>
                     </div>
                 </div>
             </div>
